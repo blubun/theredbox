319a2bd6dfc42b12ba3d285e636b48b1e7abf2fc
blubun <reyozzie23@gmail.com>
2025-01-28 22:46:42 -0500

    transferring project into a react app

---
+++ theredbox/src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="App">
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+        <link href="https://fonts.googleapis.com/css2?family=Cherry+Bomb+One&display=swap" rel="stylesheet"></link>
+       <div class="header">
+        <h1 class="title">The Red Box</h1>
+        <p class="about">About</p>  
+    </div>
+    <div id="box-container">
+       <button type="button" id="red-box">
+       </button> 
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ theredbox/src/about.js
@@ -0,0 +1,13 @@
+function About () {
+    return (
+        <div className="container">
+            <div class="header">
+                <h1 class="title">About</h1>
+                <a class="about" href="index.html">The Red Box</a>  
+            </div>
+            <div id="box-container">
+                <p>The Red Box is a small side project that begins with a simple red box. We shall see where it goes.</p>
+            </div>
+        </div>    
+    );
+}
